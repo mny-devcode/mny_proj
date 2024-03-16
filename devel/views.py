d6547627c154b45d847137b6413c586cd59e4d7d
Mihaly Nyilas <mny.devcode@gmail.com>
2024-03-16 13:23:14 +0100

    list, simplified: skills, abilities

--- devel/views.py
+++ devel/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .models import LearnPlace, Certification
-# from .models import Certification
 
-# Create your views here.
+
 def home(request):
     return render(request, "devel/home.html", {})
 
@@ -12,23 +11,26 @@ def cert(request):
     SoloLearn = Certification.objects.filter(learnt_from=1)
     GrassHopper = Certification.objects.filter(learnt_from=2)
     classname = Certification.objects.values('classname')
-#    certifications = Certification.objects.all()
     context = {
         'learn_places': learn_places,
         'SoloLearn': SoloLearn,
         'GrassHopper': GrassHopper,
         'classname': classname,
-#        'certifications': certifications
     }
     return render(request, "devel/cert.html", context)
 
 
 def cv(request):
-    return render(request, "devel/cv.html", {})
+    abilities = ['Linux', 'flexibility', 'enthusiastic', 'reliable', 'honest', 'prudent', 'wise', 'clever', 'creative', 'persistent', 'monotony tolerance', 'accurate', 'systematic', 'good team player', 'problem solving skills', 'a constant and fast learner', 'logical mind with great memory', 'keeping up-to-date knowledge', 'management of computer controlled systems', 'scheduled maintenance on my works']
+    context = {
+        'abilities': abilities,
+    }
+    return render(request, "devel/cv.html", context)
 
 
 def devel(request):
     return render(request, "devel/devel.html", {})
+
 
 def affiliates(request):
     return render(request, "devel/affiliates.html", {})
