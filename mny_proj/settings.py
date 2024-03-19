66cf3ef01f5d676178a0b459523e7583f4fb05c1
Mihaly Nyilas <mny.devcode@gmail.com>
2024-03-19 16:48:17 +0100

    add: postgresql settings

--- mny_proj/settings.py
+++ mny_proj/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,8 +64,15 @@ WSGI_APPLICATION = "mny_proj.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "mny_proj.sqlite3",
+        # "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_DB_PORT'),
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "mny_proj.sqlite3",
     }
 }
 
